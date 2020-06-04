89dd0e4548094a64978b24c10e0a8f5df639fb51
DexiGG <invokedex@gmail.com>
2020-06-05 03:28:01 +0600

    'Updated'

--- portfolio/core/views.py
+++ portfolio/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import *
+from django.contrib.auth.views import LoginView
 
 
 class HomeTemplateView(TemplateView):
--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'users.apps.UsersConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL = 'http://127.0.0.1:8000/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
